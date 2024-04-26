45c2ca8217d2e333bf47365566d856493f715734
ewouds <ewoudsmets@microsoft.com>
2024-04-26 14:22:05 +0200

    Button bar remain fixed at the bottom of the screen

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="container position-absolute bottom-0 start-50 translate-middle-x" ref="ews">
+    <div class="container position-fixed bottom-0 start-50 translate-middle-x" ref="ews">
         <div class="btn-group btn-group-lg " role="group" style="width:100%">
             <button @click="reset()" type="button" class="btn btn-secondary" id="btn-reset"
                 style="width:20%">Reset</button>
             <button type="button" class="btn btn-danger" id="btn-fail" style="width:20%">Fail</button>
-            <button type="button" class="btn btn-warning" id="btn-warning" style="width:20%">Warning</button>
-            <button @click="checked()" type="button" class="btn btn-success" id="btn-checked"
-                style="width:40%">Checked</button>
+            <button @click="checked()" type="button" class="btn btn-success" id="btn-checked" style="width:60%">
+                Checked</button>
         </div>
     </div>
 </template>
