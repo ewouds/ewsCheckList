1c226e5fe6ddc3e0cf9761afe96fa66cb66f5843
ewouds <ewoudsmets@microsoft.com>
2024-04-29 20:46:51 +0200

    added reset all via longpress

--- src/components/ActionButtons.vue
+++ src/components/ActionButtons.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container position-fixed bottom-0 start-50 translate-middle-x" ref="ews">
         <div class="btn-group btn-group-lg " role="group" style="width:100%">
-            <button @click="reset()" type="button" class="btn btn-secondary" id="btn-reset"
-                style="width:20%">Reset</button>
+            <button @click="reset()" type="button" class="btn btn-secondary" id="btn-reset" style="width:20%"
+                ref="resetBTNRef">Reset</button>
             <button @click="failed()" type="button" class="btn btn-danger" id="btn-fail" style="width:20%">Fail</button>
             <button @click="checked()" type="button" class="btn btn-success" id="btn-checked" style="width:60%">
                 Checked</button>
@@ -11,8 +11,28 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import { useCheckListStore } from '@/stores/checklist'
+import { onLongPress } from '@vueuse/core'
 const storeChecklist = useCheckListStore()
+const resetBTNRef = ref(null)
+
+onMounted(() => {
+    onLongPress(
+        resetBTNRef.value,
+        onLongPressCallbackHook,
+        {
+            modifiers: {
+                prevent: true
+            }
+        }
+    )
+})
+
+function onLongPressCallbackHook() {
+    console.log("Long press detected")
+    resetAll();
+}
 
 
 function reset() {
@@ -31,6 +51,29 @@ function reset() {
     let newActiveItem = storeChecklist.checkItems.find(check => (check.checklistID === activeItem.checklistID && check.checked === false))
     newActiveItem.active = true
     newActiveItem.checkable = true
+}
+
+function resetAll() {
+    let activeItem = storeChecklist.checkItems.find(check => check.active === true)
+    //let checkItemsInCurrentList = storeChecklist.checkItems.filter(check => check.checklistID === activeItem.checklistID)
+    //unchecked all checks in current list
+    storeChecklist.checkItems.forEach(check => {
+        check.checked = false
+        check.active = false
+        check.checkedByButton = false
+        check.failed = false
+    });
+    //reset prorgress of current lists
+    storeChecklist.checkLists.forEach(list => {
+        list.progress = 0
+        list.active = false
+    });
+    //activate first unchecked check active
+    let newActiveItem = storeChecklist.checkItems[0]
+    newActiveItem.active = true
+    newActiveItem.checkable = true
+    //activate first list
+    storeChecklist.checkLists[0].active = true
 }
 
 function checked() {
--- src/components/CheckList.vue
+++ src/components/CheckList.vue
@@ -1,13 +1,19 @@
 <template>
     <h2 :id="`heading${name}`" class="accordion-header">
-        <button v-bind:data-bs-target="`#collapse${checklist.id}`" v-bind:aria-controls="`collapse${checklist.id}`"
+        <div v-bind:data-bs-target="`#collapse${checklist.id}`" v-bind:aria-controls="`collapse${checklist.id}`"
             class="accordion-button" :class="{ collapsed: !checklist.active, show: !checklist.active }" type="button"
             data-bs-toggle="collapse" aria-expanded="true">
             {{ checklist.id }} # {{ checklist.name }} [
             {{
                 checklist.progress }}/{{
                 checklist.totalChecks }}]
-        </button>
+
+        </div>
+        <div class="progress" role="progressbar" aria-label="Example 2px high" :aria-valuenow="`${progress}`"
+            aria-valuemin="0" aria-valuemax="100" style="height: 2px">
+            <div class="progress-bar" :style="{ width: `${progress}%` }"></div>
+        </div>
+
     </h2>
     <div :id="`collapse${checklist.id}`" v-bind:aria-labelledby="`heading${checklist.id}`"
         class="accordion-collapse collapse" :class="{ show: checklist.active }" data-bs-parent="#checkListsAccordion">
@@ -31,6 +37,7 @@ const props = defineProps({
 })
 
 const checklist = storeChecklist.checkLists[storeChecklist.checkLists.findIndex(list => list.id === props.checklistId)]
+let progress = checklist.progress / checklist.totalChecks * 100
 
 defineExpose({
     // activateCheck
